b0195d22ae447685e87110d7e564fae9e6a3b9b8
Guru Dahiphale <132564849+Guruvd07@users.noreply.github.com>
2026-02-25 15:06:19 +0530

    Update adminService.ts

--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://handio.onrender.com";
+const API = "https://handio.onrender.com/api";
 
 export const getAllProviders = async (token: string) => {
   const res = await axios.get(`${API}/admin/providers`, {
