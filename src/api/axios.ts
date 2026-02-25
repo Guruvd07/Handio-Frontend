1fce2d3a21be2e31ea11121dbe641b5831e74a8f
Guru Dahiphale <132564849+Guruvd07@users.noreply.github.com>
2026-02-25 15:06:55 +0530

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://handio.onrender.com"
+  baseURL: "https://handio.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
